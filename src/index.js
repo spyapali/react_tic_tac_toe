f88e449f7fdfab66d93c27095000adecaea71a92
Shalini Pyapali <shalu.pyapali@gmail.com>
2017-06-09 23:42:06 -0700

    Add X to board when clicking on element on the board.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,16 @@ import ReactDOM from 'react-dom';
 import './index.css'
 
 class Square extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            value: null,
+        };
+    }
     render() {
         return (
-            <button className="square">
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
@@ -17,7 +23,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square />;
     }
 
     render() {
